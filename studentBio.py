cd21506b7d617e153a4b522db9401dcc227c4cc6
otimemmanuel1 <146837325+otimemmanuel1@users.noreply.github.com>
2023-10-04 07:46:08 +0300

    Create studentBio.py

---
+++ studentBio.py
@@ -0,0 +1,17 @@
+#python program to display student biodata after being entered into this program
+print('Enter your biodata please')
+students = []
+print('\n')
+for i in range(0,9):
+      name = str(input('Enter you name: '))
+      students.append(name)
+      regNo = str(input('Enter your registration number: '))
+      students.append(regNo)
+      age = int(input('Enter your age: '))
+      students.append(age)
+      course = str(input('Enter your course: '))
+      students.append(course)
+      yearOfStudy = int(input('Enter your Year of study: '))
+      students.append(yearOfStudy)
+      graduationYear = int(input('Enter your expected year of graduation: '))
+  print(students)
